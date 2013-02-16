dfde24fa22ba9c7bed7e24858379a5d92850aca4
Thomas Hunger <tehunger@gmail.com>
2013-02-16 16:50:16 +0000

    Oh hai.

---
+++ README.rst
@@ -0,0 +1,46 @@
+Ragel random output generator
+=============================
+
+This tool allows you to generate random output from a `ragel <http://www.complang.org/ragel/>`_ FSM.
+
+A useful example is fuzz testing. Assume you have a HTTP header parser
+implemented in ragel. By definition the parser will be able to parse
+the output of this tool. By flipping a few bits in the output you can
+feed your parser almost-correct data and see how it behaves.
+
+It's also useful to get a feel for possible inputs of your grammar. I
+managed to spot a few bugs just by looking at the output from this
+tool and realising that the grammar was too permissive.
+
+Usage
+=====
+
+First you need to generate the XML version of your FSM. E.g.:
+
+::
+   ragel -x smtp.ragel > smtp.xml
+
+Now you can run this tool:
+
+::
+   python rg.py smtp.xml
+
+Here's some example output for a SMTP parser I wrote a while back:
+
+::
+
+    'HELO [05.73]\n'
+    'DATA\n'
+    'EHLO 5.7eKI\r\n'
+    'QUIT\r\n'
+    'DATA\r\n'
+    'EHLO K\n'
+    'EHLO s\n'
+    'RCPT To:<+\\4"vT.4.\\"Id8".\\7.\\D8"+6"d\\J"\\A5+"F>\n'
+    'QUIT\n'
+    'DATA\n'
+    'EHLO [4.1]\n'
+    'DATA\r\n'
+    'RCPT tO:<..7>\r\n'
+    'QUIT\r\n'
+    'EHLO [58.0xfa.9]\r\n'
---
+++ rg.py
@@ -0,0 +1,56 @@
+import argparse
+import collections
+import random
+from lxml import etree
+
+T = collections.namedtuple('T', 'start end newstate')
+
+def get_state_machine(path):
+    states = {}
+    final_set = set()
+
+    tree = etree.parse(path)
+    entry = tree.find('/ragel_def/machine/entry_points/entry[1]')
+    entry_state = int(entry.text)
+
+    for state in tree.findall('/ragel_def/machine/state_list/state'):
+        id = int(state.get('id'))
+        final = state.get('final', 'f') == 't'
+        if final:
+            final_set.add(id)
+            
+        l = []
+        for t in state.findall('./trans_list/t'):
+            start, end, newstate, _ = t.text.split()
+            l.append(T(int(start), int(end), int(newstate)))
+        states[id] = l
+    return entry_state, states, final_set
+
+def run_random(e, sm, final_set):
+    s = sm[e]
+    while True:
+        t = random.choice(s)
+        
+        c = random.choice(range(t.start, t.end + 1))
+        if c < 0:
+            yield c + 128
+        else:
+            yield c
+
+        if t.newstate in final_set:
+            break
+        s = sm[t.newstate]
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    args = parser.parse_args()
+
+    e, sm, f = get_state_machine(args.path)
+    run_random(e, sm, f)
+    while True:
+        word = ''.join(chr(x) for x in run_random(e, sm, f))
+        print repr(word)
+
+if __name__ == '__main__':
+    main()
